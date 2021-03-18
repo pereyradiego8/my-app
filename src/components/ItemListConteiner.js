2ee506577300e52fa67eda551d34d594a2ba9f02
Diego Pereyra <8pereyradiego8@gmail.com>
2021-03-18 15:24:24 -0300

    contador

--- src/components/ItemListConteiner.js
+++ src/components/ItemListConteiner.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React from "react";
+import { ItemCount } from "./ItemCount";
 
-export const ItemListContainer = () => {
-  return ( <div>Lista de Componentes</div>
-  )
+export const ItemListContainer = () =>  {
+  return (
+    <div className="container ">
+      Lista de Componentes
+      <ItemCount stock="6" initial="1" />
+    </div>
+  );
 }
